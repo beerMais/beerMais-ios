9dc6f2b71f45b2a263f9fbaad95ea82d4e519afd
jnevesjunior <contato@joseneves.com.br>
2019-04-28 15:17:57 -0300

    Add filter option to resume beers screen

--- BeerMais/Controllers/ResumeBeersVC.swift
+++ BeerMais/Controllers/ResumeBeersVC.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialChips
 
 protocol ResumeBeersVCDelegate: class {
     func reloadBeers()
 }
 
-class ResumeBeersVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ResumeBeersVC: UIViewController {
     @IBOutlet weak var rankView: UIView!
     @IBOutlet weak var rankBrandLabel: UILabel!
     @IBOutlet weak var rankBeerImageView: UIImageView!
@@ -20,9 +21,14 @@ class ResumeBeersVC: UIViewController, UICollectionViewDelegate, UICollectionVie
     @IBOutlet weak var rankAmountLabel: UILabel!
     @IBOutlet weak var rankEconomyLabel: UILabel!
     @IBOutlet weak var beersCollectionView: UICollectionView!
+    @IBOutlet weak var filterCollectionView: UICollectionView!
+    @IBOutlet weak var scroll: UIScrollView!
+    @IBOutlet weak var scrollView: UIView!
     
-    var beers = [Beer]()
-    var beerP: BeerP!
+    private var beers = [Beer]()
+    private var beerP: BeerP!
+    private var filterOptions = ["Preço", "Teor Alcoólico"]
+    private var selectedFilter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +36,24 @@ class ResumeBeersVC: UIViewController, UICollectionViewDelegate, UICollectionVie
         self.rankView.layer.cornerRadius = 15
         self.setBorderToRank()
         
+        for constraint in self.view.constraints {
+            if constraint.identifier == "scrollViewHeight" {
+                constraint.constant = 45
+                self.scrollView.layoutIfNeeded()
+            }
+        }
+        
         self.beersCollectionView.register(UINib(nibName: "BeerCollectionCell", bundle: nil), forCellWithReuseIdentifier: "beerCollectionCell")
+        self.filterCollectionView.register(MDCChipCollectionViewCell.self, forCellWithReuseIdentifier: "filterCollectionCell")
         
         self.beerP = BeerP()
         self.getBeers()
         
         self.addDelegate()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.scroll.contentOffset = CGPoint(x: 0.0, y: 45.0)
     }
     
     @IBAction func newBeerAction(_ sender: Any) {
@@ -72,65 +90,12 @@ class ResumeBeersVC: UIViewController, UICollectionViewDelegate, UICollectionVie
         }, text: "Apagar")
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let lines = self.beers.count
-        var backgroundView = UIView()
-        
-        if (lines == 0) {
-            let message = UILabel()
-            message.textColor = UIColor.darkGray
-            message.text = "Clique em + para adicionar uma bebida"
-            message.textAlignment = .center
-            message.numberOfLines = 2
-            
-            backgroundView = message
-        }
-        
-        self.beersCollectionView.backgroundView = backgroundView
-        return lines
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let beer = self.beers[indexPath.row]
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "beerCollectionCell", for: indexPath) as! BeerCollectionViewCell
-        
-        cell.setAmount(amount: beer.amount)
-        cell.setBrand(brand: beer.brand ?? "")
-        cell.setValue(value: beer.value)
-        cell.setType(type: beer.type)
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-        
-        let numberOfCells = floor(self.view.frame.size.width / flowLayout.itemSize.width);
-        let edgeInsets = (self.view.frame.size.width - (numberOfCells * flowLayout.itemSize.width)) / (numberOfCells + 1);
-        
-        return UIEdgeInsets(top: 0, left: edgeInsets, bottom: 0, right: edgeInsets);
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let beer = self.beers[indexPath.row]
-        
-        let storyboard = UIStoryboard(name: "NewBeer", bundle: nil)
-        
-        let viewController = storyboard.instantiateViewController(withIdentifier: "NewBeerVCID") as! NewBeerVC
-        viewController.modalPresentationStyle = .overCurrentContext
-        viewController.attachResumeBeersDelegate(delegate: self)
-        viewController.setBeer(beer: beer)
-        
-        if self.tabBarController != nil {
-            self.tabBarController?.present(viewController, animated: true, completion: nil)
-        } else {
-            self.present(viewController, animated: true, completion: nil)
-        }
-    }
-    
     private func addDelegate() {
         self.beersCollectionView.delegate = self
         self.beersCollectionView.dataSource = self
+        
+        self.filterCollectionView.delegate = self
+        self.filterCollectionView.dataSource = self
     }
     
     private func getBeers() {
@@ -216,10 +181,110 @@ class ResumeBeersVC: UIViewController, UICollectionViewDelegate, UICollectionVie
         self.rankView.backgroundColor = backgroundColor
     }
     
+    private func getBeerCount() -> Int {
+        let lines = self.beers.count
+        var backgroundView = UIView()
+        
+        if (lines == 0) {
+            let message = UILabel()
+            message.textColor = UIColor.darkGray
+            message.text = "Clique em + para adicionar uma bebida"
+            message.textAlignment = .center
+            message.numberOfLines = 2
+            
+            backgroundView = message
+        }
+        
+        self.beersCollectionView.backgroundView = backgroundView
+        return lines
+    }
+    
+    private func getFilterCount() -> Int {
+        return self.filterOptions.count
+    }
+    
+    private func getBeerCell(_ collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+        let beer = self.beers[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "beerCollectionCell", for: indexPath) as! BeerCollectionViewCell
+        
+        cell.setAmount(amount: beer.amount)
+        cell.setBrand(brand: beer.brand ?? "")
+        cell.setValue(value: beer.value)
+        cell.setType(type: beer.type)
+        
+        return cell
+    }
+    
+    private func getFilterCell(_ collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "filterCollectionCell", for: indexPath) as! MDCChipCollectionViewCell
+        
+        cell.chipView.titleLabel.text = self.filterOptions[indexPath.row]
+        cell.chipView.isSelected = self.selectedFilter == indexPath.row
+        cell.chipView.setBorderWidth(1.0, for: .selected)
+        
+        return cell
+    }
+    
+    private func selectBeer(row: Int) {
+        let beer = self.beers[row]
+        
+        let storyboard = UIStoryboard(name: "NewBeer", bundle: nil)
+        
+        let viewController = storyboard.instantiateViewController(withIdentifier: "NewBeerVCID") as! NewBeerVC
+        viewController.modalPresentationStyle = .overCurrentContext
+        viewController.attachResumeBeersDelegate(delegate: self)
+        viewController.setBeer(beer: beer)
+        
+        if self.tabBarController != nil {
+            self.tabBarController?.present(viewController, animated: true, completion: nil)
+        } else {
+            self.present(viewController, animated: true, completion: nil)
+        }
+    }
+    
+    private func selectFilter(row: Int) {
+        self.selectedFilter = row
+        self.filterCollectionView.reloadData()
+    }
 }
 
 extension ResumeBeersVC: ResumeBeersVCDelegate {
     internal func reloadBeers() {
         self.getBeers()
+    }
+}
+
+extension ResumeBeersVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if (collectionView == self.beersCollectionView) {
+            return self.getBeerCount()
+        } else {
+            return self.getFilterCount()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if (collectionView == self.beersCollectionView) {
+            return self.getBeerCell(collectionView, indexPath: indexPath)
+        } else {
+            return self.getFilterCell(collectionView, indexPath: indexPath)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+
+        let numberOfCells = floor(self.view.frame.size.width / flowLayout.itemSize.width)
+        let edgeInsets = (self.view.frame.size.width - (numberOfCells * flowLayout.itemSize.width)) / (numberOfCells + 1)
+
+        return UIEdgeInsets(top: 0, left: edgeInsets, bottom: 0, right: edgeInsets)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if (collectionView == self.beersCollectionView) {
+            self.selectBeer(row: indexPath.row)
+        } else {
+            self.selectFilter(row: indexPath.row)
+        }
     }
 }
