66c678e5152ba86f9751cf307ecb0ef83f06ac3c
Jose Neves <contato@joseneves.com.br>
2023-11-15 15:21:18 -0300

    app clip - fix firebase init and remove lottie

--- BeerMais Clip/AppDelegate.swift
+++ BeerMais Clip/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 
+import Firebase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
