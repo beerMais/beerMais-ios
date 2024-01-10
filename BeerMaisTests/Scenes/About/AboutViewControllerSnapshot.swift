c5c54d29d73b4d914768f8551711ce6bcd40b0ee
Jose Neves <contato@joseneves.com.br>
2024-01-09 21:54:09 -0300

    Change About snapshots to use mocked remote config

--- BeerMaisTests/Scenes/About/AboutViewControllerSnapshot.swift
+++ BeerMaisTests/Scenes/About/AboutViewControllerSnapshot.swift
@@ -19,6 +19,25 @@ class AboutViewControllerSnapshot: XCTestCase {
 
     func testViewController() {
         let remoteConfig = RemoteConfigSpy()
+        
+        let remoteConfigValueStub = RemoteConfigValueStub()
+        remoteConfigValueStub.boolValue = false
+        
+        remoteConfig.configValueCacheReturn = ["is_donate_available": remoteConfigValueStub]
+        
+        let vc = AboutFactory.build(remoteConfig: remoteConfig)
+
+        assertSnapshot(of: vc, as: .image)
+    }
+
+    func testWithDonatesViewController() {
+        let remoteConfig = RemoteConfigSpy()
+        
+        let remoteConfigValueStub = RemoteConfigValueStub()
+        remoteConfigValueStub.boolValue = true
+        
+        remoteConfig.configValueCacheReturn = ["is_donate_available": remoteConfigValueStub]
+        
         let vc = AboutFactory.build(remoteConfig: remoteConfig)
 
         assertSnapshot(of: vc, as: .image)
